fcda6019512c96cfaf50b9072ccdcbf31405b59e
Weipeng Cao <weipengcao@weip.desktop.linux>
2019-11-27 19:59:55 -0700

    updated website

---
+++ precache-manifest.950cc8dc27ba560416cc36be5fe02474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011e86877898b529566cf05eaba285d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "245514ca3e4ac1ba2679",
+    "url": "/static/css/main.2f4885d2.chunk.css"
+  },
+  {
+    "revision": "5699155da11f72e4f1d5",
+    "url": "/static/js/2.f60de40d.chunk.js"
+  },
+  {
+    "revision": "245514ca3e4ac1ba2679",
+    "url": "/static/js/main.72a83c18.chunk.js"
+  },
+  {
+    "revision": "58e884bfb38d1cefa445",
+    "url": "/static/js/runtime-main.1c31b8f3.js"
+  }
+]);
